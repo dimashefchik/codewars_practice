cc2f1638d04482a29a71b04868f793f945929a54
Dima Shefchik <96975830+dimashefchik@users.noreply.github.com>
2022-03-29 23:12:04 +0300

    Update 6kyu_sums_of_parts.py

--- 6kyu_sums_of_parts.py
+++ 6kyu_sums_of_parts.py
@@ -1,14 +1,9 @@
+# The function parts_sums (or its variants in other languages) will take as parameter a list ls 
+# and return a list of the sums of its parts as defined above.
 ls = [1, 2, 3, 4, 5, 6]
 
-# result = []
-# i = 0
-# while i <= len(ls):
-#     result.append(sum(ls[i:]))
-#     i += 1
-# print(result)
-
-
-sums = [sum(ls)]
-for i in ls:
-    sums.append(sums[-1]-i)
-print(sums)
+def parts_sums(ls):
+    result = [sum(ls)]
+    for i in ls:
+        result.append(result[-1]-i)
+    return result
