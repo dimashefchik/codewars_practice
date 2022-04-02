a719d2752f2d01e97f3955bc5f457f595218d8cb
Dima Shefchik <96975830+dimashefchik@users.noreply.github.com>
2022-04-03 00:25:52 +0300

    Create 5Kyu_Extract_the_domain_name_from_a_URL.py

---
+++ 5Kyu_Extract_the_domain_name_from_a_URL.py
@@ -0,0 +1,22 @@
+# Write a function that when given a URL as a string, parses out just the domain name and returns it as a string. For example:
+
+# * url = "http://github.com/carbonfive/raygun" -> domain name = "github"
+# * url = "http://www.zombie-bites.com"         -> domain name = "zombie-bites"
+# * url = "https://www.cnet.com"                -> domain name = cnet"
+
+def domain_name(url):
+    my = url.replace('http://', '')
+    my = my.replace('https://', '')
+    
+    ar = my.split('/')
+    b = ar[0]
+    if 'www.' in b:
+        b = b.replace('www.', '')
+    if '.' in b:
+        c = b.index('.')
+        print(c)
+        b = b[0:c]
+    return b
+  
+ url = "http://github.com/carbonfive/raygun"
+ print(domain_name(url))
