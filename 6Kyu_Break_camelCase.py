d8210edc5b8abae0cd9e8906b84a859f69f5f0d0
Dima Shefchik <96975830+dimashefchik@users.noreply.github.com>
2022-04-04 02:07:01 +0300

    Create 6Kyu_Break_camelCase.py

---
+++ 6Kyu_Break_camelCase.py
@@ -0,0 +1,16 @@
+# Complete the solution so that the function will break up camel casing, using a space between words.
+
+# Example
+# "camelCasing"  =>  "camel Casing"
+# "identifier"   =>  "identifier"
+# ""             =>  ""
+
+def solution(s):
+    result = []
+    for i in s:
+        if i.isupper():
+            result.append(' ')
+            result.append(i)
+        else:
+            result.append(i)
+    return ''.join(result)
