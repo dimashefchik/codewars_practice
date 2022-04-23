47ccdbda81cbeceb2fc9cd5cd6407fe2f30c373e
Dima Shefchik <96975830+dimashefchik@users.noreply.github.com>
2022-04-23 23:37:07 +0300

    Create 6Kyu_Find_The_Parity_Outlier.py

---
+++ 6Kyu_Find_The_Parity_Outlier.py
@@ -0,0 +1,25 @@
+# You are given an array (which will have a length of at least 3, but could be very large) containing integers. 
+# The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N. 
+# Write a method that takes the array as an argument and returns this "outlier" N.
+# Examples
+# [2, 4, 0, 100, 4, 11, 2602, 36]
+# Should return: 11 (the only odd number)
+# [160, 3, 1719, 19, 11, 13, -21]
+# Should return: 160 (the only even number)
+
+def find_outlier(integers):
+    count_odd = 0
+    count_even = 0
+    for i in range(3):
+        if integers[i] % 2 == 0:
+            count_even += 1
+        else:
+            count_odd += 1
+    if count_even > count_odd:
+        return int(''.join([str(x) for x in integers if x % 2 == 1]))
+    else:
+        return int(''.join([str(x) for x in integers if x % 2 == 0]))
+
+      
+n =   [160, 3, 1719, 19, 11, 13, -21]    
+print(find_outlier(n))
